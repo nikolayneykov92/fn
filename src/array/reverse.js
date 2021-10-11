1740fee0a21c93754fcbcf5abf00ee29af2b498b
Teodora <teodora.zaharieva@gtmhub.com>
2021-10-11 20:33:12 +0300

    added reverse fn

---
+++ src/array/reverse.js
@@ -0,0 +1,32 @@
+const set = require('../_helpers/set')
+const get = require('../_helpers/get')
+
+function reverse(array) {
+  const reversedArray = []
+  let leftIndex = -1
+  let rightIndex = array.length
+
+  while (++leftIndex <= --rightIndex) {
+    set(reversedArray, leftIndex, get(array, rightIndex))
+    set(reversedArray, rightIndex, get(array, leftIndex))
+  }
+
+  return reversedArray
+}
+
+const numbers = [1, 2, 3, 4, 5]
+const reversedNumbers = reverse(numbers)
+console.log(reversedNumbers) // [5, 4, 3, 2, 1]
+console.log(numbers) //[1, 2, 3, 4, 5]
+console.log(reversedNumbers !== numbers) // true
+
+/**
+  
+[_, _, _, _, _]
+[5, _, _, _, _]
+[5, _, _, _, 1]
+[5, 4, _, _, 1]
+[5, 4, _, 2, 1]
+[5, 4, 3, 2, 1]
+[5, 4, 3, 2, 1]
+ */
